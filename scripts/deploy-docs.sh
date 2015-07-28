2a65755e2a1fb031eafdee68e3d981efcc25b12c
Matthias Kuhn <matthias@opengis.ch>
2015-07-28 23:36:41 +0200

    Don't do jekyll

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -12,6 +12,7 @@ git clone git@github.com:QGEP/docs.git --branch gh-pages
 cd docs
 git rm . -r
 cp ../../build/html/* . -r
+touch .nojekyll
 git add -A
 git commit -m "Automatic update from https://github.com/qgep/docs/commit/${TRAVIS_COMMIT}"
 git push
