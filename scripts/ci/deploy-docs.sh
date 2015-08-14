32e3bb07f200a30c5491a5b85f0f16ffd572844e
Matthias Kuhn <matthias@opengis.ch>
2015-08-14 23:23:10 +0200

    Deploy docs in after_success

--- scripts/ci/deploy-docs.sh
+++ scripts/ci/deploy-docs.sh
@@ -8,17 +8,17 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
   echo "publish website";
-fi
 
-pushd ${DIR}
-mkdir publish
-cd publish
-git clone git@github.com:QGEP/docs.git --branch gh-pages
-cd docs
-git rm . -r
-cp ../../build/html/* . -r
-touch .nojekyll
-git add -A
-git commit -m "Automatic update from https://github.com/qgep/docs/commit/${TRAVIS_COMMIT}"
-git push
-popd
+  pushd ${DIR}
+  mkdir publish
+  cd publish
+  git clone git@github.com:QGEP/docs.git --branch gh-pages
+  cd docs
+  git rm . -r
+  cp ../../build/html/* . -r
+  touch .nojekyll
+  git add -A
+  git commit -m "Automatic update from https://github.com/qgep/docs/commit/${TRAVIS_COMMIT}"
+  git push
+  popd
+fi
