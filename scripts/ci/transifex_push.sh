3ee4adc4e771b2966512378dd79d47a1c91a9744
Matthias Kuhn <matthias@opengis.ch>
2017-03-26 14:09:32 +0200

    [travis] No transifexing if not on master

--- scripts/ci/transifex_push.sh
+++ scripts/ci/transifex_push.sh
@@ -4,4 +4,6 @@ then
   echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = $TRANSIFEX_USERNAME\npassword = $TRANSIFEX_PASSWORD\ntoken =\n" > ~/.transifexrc
   make gettext
   tx push -s
+else
+  echo "  Not on master branch. Will not push to transifex."
 fi
