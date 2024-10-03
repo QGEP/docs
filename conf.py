a7c030d53406ba31da45f1b92870b357cba45725
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2024-10-03 13:39:38 +0200

    Fix flake8
    Fix flake8

--- conf.py
+++ conf.py
@@ -186,7 +186,7 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     "papersize": "a4paper",
     # The font size ('10pt', '11pt' or '12pt').
-    #'pointsize': '10pt',
+    # 'pointsize': '10pt',
     # Additional stuff for the LaTeX preamble.
     "preamble": """\\usepackage{combelow}
     \\usepackage{newunicodechar}
